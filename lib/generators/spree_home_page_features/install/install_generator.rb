0cfac6bd4aa342c419a439ed1f3f95e0e2d21adc
Sebastián Vicencio <sivicencio@acid.cl>
2015-06-18 17:11:21 -0300

    Update install_generator with new asset structure (optional)

--- lib/generators/spree_home_page_features/install/install_generator.rb
+++ lib/generators/spree_home_page_features/install/install_generator.rb
@@ -3,13 +3,21 @@ module SpreeHomePageFeatures
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/frontend/all.js', "//= require frontend/spree_home_page_features\n"
-        append_file 'app/assets/javascripts/backend/all.js', "//= require backend/spree_home_page_features\n"
+        res = ask 'Would you like to append spree_home_page_features to the JS manifest? [Y/n]'
+         if res == '' || res.downcase == 'y'
+          append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_home_page_features\n"
+         else
+          puts "Don't forget to require and initialize Flexslider dependency. Use of Bower highly recommended"
+         end
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/frontend/all.css', " *= require frontend/spree_home_page_features\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/backend/all.css', " *= require backend/spree_home_page_features\n", :before => /\*\//, :verbose => true
+        res = ask 'Would you like to append spree_home_page_features to the CSS manifest? [Y/n]'
+        if res == '' || res.downcase == 'y'
+          inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', "*= require spree/frontend/spree_home_page_features\n", :before => /\*\//, :verbose => true
+        else
+          puts "Don't forget to add your own styles to the slider. You can also use the Flexslider base styles (use of Bower highly recommended)"
+        end
       end
 
       def add_migrations
@@ -17,12 +25,12 @@ module SpreeHomePageFeatures
       end
 
       def run_migrations
-         res = ask 'Would you like to run the migrations now? [Y/n]'
-         if res == '' || res.downcase == 'y'
-           run 'bundle exec rake db:migrate'
-         else
-           puts 'Skipping rake db:migrate, don\'t forget to run it!'
-         end
+        res = ask 'Would you like to run the migrations now? [Y/n]'
+        if res == '' || res.downcase == 'y'
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
