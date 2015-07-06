c9654fb666a4e2169d3f7a663c891da1eb3f988c
Marcelo Espina <mespina@acid.cl>
2015-07-06 12:14:40 -0300

    fix AddBannerCategoryToSpreeBanners migration

--- db/migrate/20150703174742_add_banner_category_to_spree_banners.rb
+++ db/migrate/20150703174742_add_banner_category_to_spree_banners.rb
@@ -1,5 +1,6 @@
 class AddBannerCategoryToSpreeBanners < ActiveRecord::Migration
   def change
-    add_reference :spree_banners, :banner_category, index: true, foreign_key: true
+    add_reference :spree_banners, :banner_category, index: true
+    add_foreign_key :spree_banners, :spree_banner_categories, column: :banner_category_id
   end
 end
